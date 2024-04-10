4a8f21e7a1a4f8d9536db67dea015096b037ba76
Jardel-Faustino <jardelfaustino07@gmail.com.com>
2024-04-09 22:18:36 -0300

    Atual

--- gtc_app/views.py
+++ gtc_app/views.py
@@ -14,25 +14,34 @@ def principal (request):
 
 def cadastrar (request):
     
-    return render(request, 'gtc_app/rotas/cadastrar.html', {'paises': paises})  # Renderiza o template 'cadastrar.html' com o formulário e passando 'paises' para o contexto do template
+    return render(request, 'gtc_app/rotas/cadastrar.html', {'pagina_ativa': 'cadastrar','paises': paises})  # Renderiza o template 'cadastrar.html' com o formulário e passando 'paises' para o contexto do template
 
 def salvos(request):
-    # Captura os dados do formulário
-    nome_us = request.POST.get("nome")
-    data_nascimento_us = request.POST.get("data_nascimento")
-    email_us = request.POST.get("email")
-    pais_us = request.POST.get("pais")  # Obter o nome do país
-    # Verifica se o e-mail já está em uso
+    if request.method == 'POST':
+        # Captura os dados do formulário
+        nome_us = request.POST.get("nome")
+        data_nascimento_us = request.POST.get("data_nascimento")
+        email_us = request.POST.get("email")
+        pais_us = request.POST.get("pais")  # Obter o nome do país
+
         # Verifica se o e-mail já está em uso
-    if usuario.objects.filter(email=email_us).exists():
-        mensagem_erro = "Este e-mail já está em uso. Por favor, escolha outro e-mail."
-        return render(request, "gtc_app/rotas/cadastrar.html", {'paises': paises, "mensagem_erro": mensagem_erro})
-    # Obter a instância do país correspondente ao nome fornecido
-    pais_instancia = pais.objects.get(nome=pais_us)
-    # Cria um novo objeto usuario com todos os campos preenchidos
-    novo_usuario = usuario.objects.create(nome=nome_us, data_nascimento=data_nascimento_us, email=email_us, pais=pais_instancia)
+        if usuario.objects.filter(email=email_us).exists():
+            mensagem_erro = "Este e-mail já está em uso. Por favor, escolha outro e-mail."
+            return render(request, "gtc_app/rotas/cadastrar.html", {'paises': paises, "mensagem_erro": mensagem_erro})
+
+        # Obter a instância do país correspondente ao nome fornecido
+        pais_instancia = pais.objects.get(nome=pais_us)
+
+        # Cria um novo objeto usuario com todos os campos preenchidos
+        novo_usuario = usuario.objects.create(nome=nome_us, data_nascimento=data_nascimento_us, email=email_us, pais=pais_instancia)
+        
+        return render(request, "gtc_app/global/principal.html", {'pagina_ativa': 'principal',"usuarios": usuarios})
     # Recupera todos os usuários após criar o novo usuário
-    return render(request, "gtc_app/global/principal.html", {"usuarios": usuarios})
+    usuarios = []
+    nome_pesquisado = request.GET.get('nome')
+    if nome_pesquisado:
+        usuarios = usuario.objects.filter(nome=nome_pesquisado)
+    return render(request, "gtc_app/rotas/pesquisar.html", {'usuarios': usuarios, 'pagina_ativa': 'pesquisar'})
 
 def atualizar (request):
     return render(request, "gtc_app/rotas/atualizar.html", {'pagina_ativa': 'atualizar'})
@@ -41,7 +50,7 @@ def deletar (request):
     return render(request, "gtc_app/rotas/deletar.html", {'pagina_ativa': 'deletar'})
 
 def pesquisar (request):
-    return render(request, "gtc_app/rotas/pesquisar.html", {'pagina_ativa': 'pesquisar'})
+    return render(request, "gtc_app/rotas/pesquisar.html", {'usuarios': usuarios, 'pagina_ativa': 'pesquisar'})
 
 def sucesso(request):
     return render(request, 'sucesso.html')
